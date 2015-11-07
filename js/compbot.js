cf2a259e2d18a6ab1840b5c76537c932d2582ba1
Tzu-Yin Hong <lili668668@gmail.com>
2015-11-07 17:49:45 +0800

    修正random問題

--- js/compbot.js
+++ js/compbot.js
@@ -4,7 +4,7 @@ var rand = require('random-js')();
 exports.res = function(str) {
     if (computer[str]) {
         var temp = computer[str];
-        return temp[rand.integer(0,temp.length)];
+        return temp[rand.integer(0,temp.length-1)];
     } else {
         return str;
     }
