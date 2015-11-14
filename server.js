1c984d332aa8d49205bab67f970a39642a20c09e
Tzu-Yin Hong <lili668668@gmail.com>
2015-11-14 20:10:47 +0800

    修正錯誤

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var config = cc();
 
 var content = fs.readFileSync('index.html');
 
+var res = "";
+
 app.use( bodyparser.json() );
 app.use( bodyparser.urlencoded({
     extended: true
@@ -28,14 +30,14 @@ app.post('/',function(request,response){
     var compbot = require('./js/compbot.js');
     var str = compbot.res(xss(request.body.talk));
     response.writeHeader(200,{'Content-Type':'text/html'});
+    res += '<div class="frame-right"><div class="me">' + xss(request.body.talk) + '</div></div>';
+    res += '<div  class="frame-left"><div class="text">' + str + '</div></div>';
+    response.write(res);
     response.write(content);
-    str = '<div class="text">' + str + '</div>';
-    //    response.write(str);
     response.end();
-    $("body").append(str);
 
 });
-
 server.listen(config.get('PORT'), config.get('IP'), function () {
-    console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+    console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT')  )
+
 });
