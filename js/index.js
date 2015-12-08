9e45eb163c3958c002c505cfccdad6c238eadd6f
Tzu-Yin Hong <lili668668@gmail.com>
2015-12-08 15:07:31 +0800

    加入輸入姓名的表單系統

--- js/index.js
+++ js/index.js
@@ -1,28 +1,43 @@
-var socket = io();
-$('form').submit(function(){
-    var m = $('#talk').val();
-    socket.emit('message', m);
-    $('#messages').append(
-        $('<div class="frame-right">').append(
-            $('<div class="me">').text(m)
-        )
-    );
-    $('#talk').val('');
-    return false;
+var name = "";
+$('#naform').submit(function(){
+    var n = $('#name').val();
+    if (n !== '') {
+        console.log(n);
+        name = n;
+        $('#dark').css("display" , "none");
+        chat();
+        return false;
+    } else {
+        console.log(n);
+        $('#warn').css("display" , "block");
+    }
 });
-socket.on('message', function(msg){
-    $('#messages').append(
-        $('<div class="frame-left">').append(
-            $('<div class="text">').text(msg)
-        )
-    );
-});
+function chat() {
+    var socket = io();
+    $('#form').submit(function(){
+        var m = $('#talk').val();
+        socket.emit('message', m);
+        $('#messages').append(
+            $('<div class="frame-right">').append(
+                $('<div class="me">').text(m)
+            )
+        );
+        $('#talk').val('');
+        return false;
+    });
+    socket.on('message', function(msg){
+        $('#messages').append(
+            $('<div class="frame-left">').append(
+                $('<div class="text">').text(msg)
+            )
+        );
+    });
 
-socket.on('bot', function(msg){
-    $('#messages').append(
-        $('<div class="frame-left">').append(
-            $('<div class="text">').text(msg)
-        )
-    );
-});
-
+    socket.on('bot', function(msg){
+        $('#messages').append(
+            $('<div class="frame-left">').append(
+                $('<div class="text">').text(msg)
+            )
+        );
+    });
+}
