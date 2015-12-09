6825b5bee4247be7c07ac5f027ffbc35a2754356
Tzu-Yin Hong <lili668668@gmail.com>
2015-12-09 10:35:32 +0800

    回復6d37595

--- test.js
+++ test.js
@@ -19,8 +19,13 @@ app.get('/', function(request,response){
 
 io.on('connection', function(socket){
     socket.on('message', function(msg){
-        io.emit('message', compbot.res(msg));
+        socket.broadcast.emit('message', msg);
+        io.emit('bot', compbot.res(msg));
     });
+});
+
+server.listen(config.get('PORT'), config.get('IP'), function () {
+    console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT')  )
 });
 
 server.listen("8080", "127.0.0.1", function () {
